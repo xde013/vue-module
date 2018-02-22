b40646bccd6958ff996562dbd4506c70cfad0a3c
Ryan Bourhil <bourhilr@gmail.com>
2018-02-22 22:22:26 +0000

    Add lodash and inject it as instance method

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import swal from 'sweetalert'
+import lodash from 'lodash'
+Object.defineProperty(Vue.prototype, '$lodash', { value: lodash }) // Make lodash available as an instance method for all components
 
 
 new Vue({
